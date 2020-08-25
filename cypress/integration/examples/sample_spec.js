d85c6313e6af17ffefcf838043f80bd0bfa21ce4
myk245 <myk245@nyu.edu>
2020-08-25 18:42:15 -0400

    adds different types of tests

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -1,6 +1,39 @@
-describe('My First Test', () => {
-   it('Does not do much!', () => {
-      expect(true).to.equal(true)
+// sample test
+// describe('My First Test', () => {
+//    it('Does not do much!', () => {
+//       expect(true).to.equal(true)
+//    })
+// })
+
+describe('Visit Test', () => {
+   it('Visits the Kitchen Sink', () => {
+      cy.visit('https://example.cypress.io')
    })
 })
 
+describe('Find Test', () => {
+   it('finds the content "type"', () => {
+      cy.visit('https://example.cypress.io')
+
+      cy.contains('type')
+   })
+})
+
+describe('Click Test', () => {
+   it('clicks the link "type"', () => {
+      cy.visit('https://example.cypress.io')
+
+      cy.contains('type').click()
+   })
+})
+
+describe('Assertion Test', () => {
+   it('clicking "type" navigates to a new url', () => {
+      cy.visit('https://example.cypress.io')
+
+      cy.contains('type').click()
+
+      // Should be on a new URL which includes '/commands/actions'
+      cy.url().should('include', '/commands/actions')
+   })
+})
